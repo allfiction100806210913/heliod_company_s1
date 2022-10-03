f420422ee5cda2d9b7e59cb4906c8f8405f97d56
allfiction100806210913 <lili10080621@gmail.com>
2022-10-03 11:07:37 +0900

    update: code

--- app/controllers/librarys_controller.rb
+++ app/controllers/librarys_controller.rb
@@ -2,5 +2,12 @@ class LibrarysController < ApplicationController
  def index
   @library = Library.all
  end
- 
+
+ def new
+  @library = Library.new
+ end
+
+ def create
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "librarys#index"
+  resources :librarys
 
 end
