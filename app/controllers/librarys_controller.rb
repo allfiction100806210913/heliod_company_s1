98218c7e7b49fe46d05027449dfc28e0bdb10bca
allfiction100806210913 <lili10080621@gmail.com>
2022-10-04 19:08:55 +0900

    update:library

--- app/controllers/librarys_controller.rb
+++ app/controllers/librarys_controller.rb
@@ -1,6 +1,9 @@
 class LibrarysController < ApplicationController
+  before_action :library_set, except: [:index, :new, :create]
+
+
  def index
-  @librarys = Library.all
+  @librarys = Library.order("created_at DESC")
  end
 
  def new
@@ -17,12 +20,26 @@ class LibrarysController < ApplicationController
  end
  
  def show
-  @library = Library.find(params[:id])
+ end
+
+ def edit
+ end
+
+ def update
+  if @library.update(library_params)
+    redirect_to library_path
+  else
+    render :edit
+  end
  end
 
  private
  def library_params
   params.require(:library).permit(:card_name, :manacost, :card_text, :color, :power, :toughness, :image, :card_type).merge(user_id: current_user.id)
+ end
+
+ def library_set
+  @library = Library.find(params[:id])
  end
 
 end
